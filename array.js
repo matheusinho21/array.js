4580ad8ffa2932af7cabae4a9389243b4ab89f65
projeto 1 <129951535+matheusinho21@users.noreply.github.com>
2024-06-05 09:02:09 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,4 @@
+const produtos=["pão", "arroz", "milho", "feijão", "café" , "salgadinho" , "bolacha", "refrigerante", "alho", "cebola" ]
+console.log(produtos);
+console.log(produtos[3]);
+console.log (produtos.length);
